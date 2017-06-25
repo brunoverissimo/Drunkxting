bab6532ad1f4f2aa447c8edec4013e504f049d8c
Bruno Verissimo <brunoverissimo1986@hotmail.com>
2017-06-25 15:45:47 -0400

    loading screen

---
+++ Assets/Scripts/MonoSingleton.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T: MonoSingleton<T>, new() {
+
+    static protected T instance;
+
+
+
+    /*
+  *------------------------------------
+    Pure virtual functions.
+  *------------------------------------
+  */
+    public abstract void OnStart();
+    public abstract void OnUpdate();
+    //====================================
+
+
+    /*
+     *------------------------------------
+       Virtual functions.
+     *------------------------------------
+     */
+    public virtual void OnAwake() { }
+
+    public virtual void OnFixedUpdate() { }
+    //====================================
+
+
+   // public abstract void StartChild();
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+
+        OnStart();
+
+	}
+
+
+    void Update()
+    {
+
+
+        OnUpdate();
+    }
+
+}
